e7a7bafd6994e7327314a848cda63ed45703eb69
Nicolas G. Querol <nicolas.gquerol@gmail.com>
2018-01-29 01:11:13 +0100

    Update gitignore

---
+++ Diurna/SelfSizingTextView.swift
@@ -0,0 +1,77 @@
+//
+//  SelfSizingTextView.swift
+//  Diurna
+//
+//  Created by Nicolas Gaulard-Querol on 08/01/2018.
+//  Copyright © 2018 Nicolas Gaulard-Querol. All rights reserved.
+//
+
+import Cocoa
+
+class SelfSizingTextView: NSTextView {
+
+    // MARK: Properties
+
+    override var intrinsicContentSize: NSSize {
+        guard
+            let container = textContainer,
+            let manager = layoutManager
+        else {
+            return NSSize(width: NSView.noIntrinsicMetric, height: NSView.noIntrinsicMetric)
+        }
+
+        let availableWidth = frame.width - textContainerInset.width
+
+        container.size = NSSize(width: availableWidth, height: .greatestFiniteMagnitude)
+        manager.ensureLayout(for: container)
+
+        let size = manager.usedRect(for: container)
+
+        return NSSize(
+            width: size.width + textContainerInset.width,
+            height: size.height + textContainerInset.height
+        )
+    }
+
+    override var textContainerOrigin: NSPoint {
+        var origin = super.textContainerOrigin
+
+        origin.x -= textContainerInset.width / 2.0
+        origin.y -= textContainerInset.height / 2.0
+
+        return origin
+    }
+
+    override var string: String {
+        didSet {
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    var attributedStringValue: NSAttributedString {
+        get {
+            return attributedString()
+        }
+
+        set {
+            textStorage?.setAttributedString(newValue)
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    // MARK: Methods
+
+    override func layout() {
+        super.layout()
+
+        if frame.size != intrinsicContentSize {
+            invalidateIntrinsicContentSize()
+        }
+    }
+
+    override func didChangeText() {
+        super.didChangeText()
+
+        invalidateIntrinsicContentSize()
+    }
+}
